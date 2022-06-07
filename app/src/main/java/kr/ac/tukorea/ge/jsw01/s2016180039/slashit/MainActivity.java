7d04c3007a5c67bf16014cd9300a4d2ab682f0f0
Seungwon <seungwon604@gmail.com>
2022-06-07 18:00:05 +0900

    Skeleton

---
+++ app/src/main/java/kr/ac/tukorea/ge/jsw01/s2016180039/slashit/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.jsw01.s2016180039.slashit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
